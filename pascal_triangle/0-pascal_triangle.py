8f59bf4153c83b1c4e80b26aa9f89f590061e29c
Litoxam <litobernier@gmail.com>
2026-05-12 11:15:56 +0200

    feat: Pascal Triangle function + README

---
+++ pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+def pascal_triangle(n):
+    """A list of lists of int representing the Pascal's triangle of n"""
+    if n <= 0:
+        return
+    else:
+        pascal_list = [[1]]
+    for i in range(1, n):
+        past_list = pascal_list[-1]
+        new_list = [1]
+
+        for j in range(1, i):
+            new_list.append(past_list[j-1] + past_list[j])
+        new_list.append(1)
+
+        pascal_list.append(new_list)
+
+    return pascal_list
