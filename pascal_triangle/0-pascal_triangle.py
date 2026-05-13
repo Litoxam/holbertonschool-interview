8c9b9e77978d5cc2dcca545330e24aa08866a131
Litoxam <litobernier@gmail.com>
2026-05-13 10:18:42 +0200

    fix: shebang fixed

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
 def pascal_triangle(n):
     """A list of lists of int representing the Pascal's triangle of n"""
@@ -17,3 +17,12 @@ def pascal_triangle(n):
         pascal_list.append(new_list)
 
     return pascal_list
+
+def print_triangle(triangle):
+    """
+    Print the triangle
+    """
+    for row in triangle:
+        print("[{}]".format(",".join([str(x) for x in row])))
+
+print_triangle(pascal_triangle(5))
