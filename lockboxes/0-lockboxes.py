8025d51135be8e7e2bde01a16aa8259abe7a2f7a
Litoxam <litobernier@gmail.com>
2026-05-22 11:53:34 +0200

    feat: Lockboxes, checks if all the boxes can be unlocked

---
+++ lockboxes/0-lockboxes.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+def canUnlockAll(boxes):
+    to_open = [0]
+    open_boxes = [0]
+    while len(to_open) > 0:
+        box = to_open.pop(0)
+
+        for key in boxes[box]:
+            if key not in open_boxes:
+                open_boxes.append(key)
+                to_open.append(key)
+    
+    if len(open_boxes) == len(boxes):
+        return True
+    else:
+        return False
+
+
+if __name__ == "__main__":
+    boxes = [[1], [2], [3], [4], []]
+    print(canUnlockAll(boxes))
+
+    boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
+    print(canUnlockAll(boxes))
+
+    boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
+    print(canUnlockAll(boxes))
