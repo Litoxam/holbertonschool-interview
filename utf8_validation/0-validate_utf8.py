771042f3e49fa5ab83c0ba5ca1522d93cbbc06cf
Litoxam <litobernier@gmail.com>
2026-08-04 15:36:58 +0200

    parse the bytes and look for continuation bytes

--- utf8_validation/0-validate_utf8.py
+++ utf8_validation/0-validate_utf8.py
@@ -11,4 +11,27 @@ def validUTF8(data):
     for num in data:
         byte = num & 0xFF
 
+        if bytes_left == 0:
+
+            if (byte >> 7) == 0:
+                continue
+
+            elif (byte >> 5) == 0b110:
+                bytes_left = 1
+
+            elif (byte >> 4) == 0b1110:
+                bytes_left = 2
+
+            elif (byte >> 3) == 0b11110:
+                bytes_left = 3
+
+            else:
+                return False
+
+        else:
+        if (byte >> 6) != 0b10:
+            return False
+
+        bytes_left -= 1
+        
     return True
